36b8545b378c3a4cbd9003961f534b07a86daa00
careyl96 <careyl96@gmail.com>
2018-03-09 13:22:42 -0800

    completed + first refactor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,37 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var keyValueTuple = [k, v]
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  }
+  
+  this._storage.get(index).push(keyValueTuple);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var targetBucket = this._storage.get(index);
+  var result;
+  
+  _.each(targetBucket, function(keyValueTuple) {
+    if (keyValueTuple[0] === k) {
+      result = keyValueTuple[1];
+    }
+  });
+  
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var targetBucket = this._storage.get(index);
+
+  _.each(targetBucket, function(keyValueTuple) {
+    if (keyValueTuple[0] === k) {
+      targetBucket = targetBucket.splice(keyValueTuple, 1);
+    }
+  });
 };
 
 
