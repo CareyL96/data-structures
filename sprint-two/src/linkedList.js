27d877d984841c3eac0fa59611adcbc8b8acf286
careyl96 <careyl96@gmail.com>
2018-03-08 16:16:48 -0800

    completed linked list and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,36 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var removedHeadValue = list.head.value;
+    list.head = list.head.next;
+    return removedHeadValue;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || list.head;
+    if (node.value === target) {
+      return true;
+    }
+    
+    node = node.next;
+    if (node !== null) {
+      return list.contains(target, node);
+    }
+    return false;
+    
   };
 
   return list;
@@ -27,3 +49,7 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var list = LinkedList();
+list.addToTail()
+list.contains(4);
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,12 +11,21 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-};
+  this.children.push(Tree(value));
+}
 
-treeMethods.contains = function(target) {
-};
-
-
+treeMethods.contains = function(target, node) {
+  node = node || this
+  if (node.value === target) { return true }
+  
+  for (var i = 0; i < node.children.length; i++) {
+    if (node.contains(target, node.children[i])) { return true };
+  }
+  
+  return false;
+  
+  //return node.contains(target, );
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
