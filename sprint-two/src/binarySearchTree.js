3cd0ead75e6ee50faaedeb9fe6199f59982bb835
careyl96 <careyl96@gmail.com>
2018-03-09 16:25:57 -0800

    Completed everything in  binarySearchTree but depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value) {
+  var treeObj = Object.create(BinarySearchTree.prototype);
+  
+  treeObj.value = value;
+  treeObj.left = null;
+  treeObj.right = null; 
+  
+  return treeObj;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+// base case:
+  // check if value is greater than this.value
+    // if this.right is null 
+      // if it is, set this.right to BinarySearchTree(value);
+// recursive case 
+    // if this.right is NOT null
+      // this.right.insert(value)
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  }
+  
+  if (value < this.value && this.left !== null) {
+    return this.left.contains(value);
+  } else if (value > this.value && this.right !== null) {
+    return this.right.contains(value);
+  }
+
+  return false;
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var rootNode = BinarySearchTree(8);
+
+rootNode.insert(5);
